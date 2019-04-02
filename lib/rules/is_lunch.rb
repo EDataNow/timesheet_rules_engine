2b5acf7d8c4bccf0eb5413d505f3bbbe16c16fb3
Shawn Lee-Kwong <splitnova@gmail.com>
2019-04-02 14:12:34 -0400

    is lunch

---
+++ lib/rules/is_lunch.rb
@@ -0,0 +1,41 @@
+require 'rules/base'
+
+module Rules
+  class IsLunch < Base
+    DEFAULTS = {  minimum_daily_hours: 0.0,
+                  maximum_daily_hours: 0.0,
+                  minimum_weekly_hours: 0.0,
+                  maximum_weekly_hours: 0.0,
+                  overtime_days: ["saturday", "sunday"],
+                  saturdays_overtime: true,
+                  sundays_overtime: true,
+                  holidays_overtime: true,
+                  decimal_place: 2,
+                  billable_hour: 0.25,
+                  closest_minute: 8.0,
+                  scheduled_shift: nil
+                }
+
+    attr_reader :criteria, :activity, :processed_activity, :base
+
+    def initialize(base, activity=nil, criteria=nil)
+      if base
+        @activity = base.activity
+        @criteria = base.criteria
+        @processed_activity = base.processed_activity
+        @base = base
+      else
+        super(activity, criteria)
+      end
+    end
+
+    def process_activity
+      if @activity.type == "lunch"
+        @processed_activity[:payable] = 0.0
+        @processed_activity[:billable] = @activity.total_hours
+      end
+
+      @processed_activity
+    end
+  end
+end
---
+++ spec/rules/is_lunch_spec.rb
@@ -0,0 +1,49 @@
+require 'rspec'
+require 'rules/is_lunch'
+
+module Rules
+  describe IsLunch do
+    describe 'processing' do
+      let(:criteria) {
+         {
+            minimum_daily_hours: 0.0,
+            maximum_daily_hours: 0.0,
+            minimum_weekly_hours: 0.0,
+            maximum_weekly_hours: 0.0,
+            saturdays_overtime: true,
+            sundays_overtime: true,
+            holidays_overtime: true,
+            decimal_place: 2,
+            billable_hour: 0.25,
+            closest_minute: 8.0,
+            scheduled_shift: OpenStruct.new(started_at: DateTime.parse("2018-01-01 6:00am"),
+                                            ended_at: DateTime.parse("2018-01-01 4:30pm"))
+          }
+      }
+
+      context 'when activity is lunch' do
+        let(:lunch) { IsLunch.new(nil, OpenStruct.new(attributes_for(:activity, type: "lunch")), criteria) }
+        subject { lunch.process_activity }
+
+        it "should calculate payable to be the same as total hours" do
+          expect(subject.billable).to eq(1.0)
+          expect(subject.payable).to eq(0.0)
+          expect(subject.total).to eq(1.0)
+        end
+      end
+
+      context 'when activity is not lunch' do
+        let(:lunch) { IsLunch.new(nil, OpenStruct.new(attributes_for(:activity)), criteria) }
+        subject { lunch.process_activity }
+
+        it "should not calculate payable total to be the same as total hours" do
+          expect(subject.payable).to eq(0.0)
+          expect(subject.billable).to eq(0.0)
+          expect(subject.total).to eq(1.0)
+        end
+      end
+
+
+    end
+  end
+end
