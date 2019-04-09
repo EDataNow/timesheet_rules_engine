20bb51b9040e6e9f2768954863f82fa04c06b96d
Shawn Lee-Kwong <splitnova@gmail.com>
2019-04-09 08:59:53 -0400

    more cleanup

--- lib/timesheet_rules_engine.rb
+++ lib/timesheet_rules_engine.rb
@@ -4,22 +4,18 @@ require 'processors/timesheets'
 class TimesheetRulesEngine
   DEFAULTS = { shift: nil, criteria: [], include_rules: [], exclude_rules: [] }
 
-  attr_reader :current_weekly_hours, :gets_bonus_overtime, :total_overtime, :total_hours, :left_early
+  attr_reader :current_weekly_hours, :left_early
 
   def initialize(timesheets=[], options={})
     @timesheets = timesheets
     @options = DEFAULTS.merge(options.symbolize_keys)
 
     @current_weekly_hours = 0.0
-    @total_hours = 0.0
-    @total_overtime = 0.0
-    @gets_bonus_overtime = true
     @left_early = false
   end
 
   def process_timesheets
     if @timesheets.any? {|t| t.left_early }
-      @gets_bonus_overtime = false
       @left_early = true
     end
 
